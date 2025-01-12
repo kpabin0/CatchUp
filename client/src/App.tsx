7dd40e514fd0f2e81917a67add8a33fb5cc8e350
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-12 20:25:51 +0545

    add: basic frontend init

---
+++ client/src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion"
+import { TbHandLoveYou } from "react-icons/tb";
+
+const _tempObject = [11, 22, 33];
+
+function App() {
+
+  const [reqData, setReqData] = useState();
+
+  useEffect(() => {
+    const req =  async () => {
+      return fetch("http://localhost:8080/")
+              .then((res) => res.json())
+              .then((data) => { setReqData(data); return data;  });
+    }
+
+    req();
+  })
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-evenly items-center">
+      <motion.h1
+        className="text-theme text-6xl font-bold uppercase font-mono"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+            duration: 0.4,
+            scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+        }}
+      >
+        Catchup <TbHandLoveYou className="inline-block" />
+      </motion.h1>
+      <div className=" w-[80%] flex flex-row justify-evenly items-center flex-wrap ">
+        {
+          _tempObject.map((ind) => {
+            return <MotionCardExample key={ind} ind={ind} />
+          })
+        }
+      </div>
+      <span>Fetched from backend <span className="text-theme-w bg-theme p-2">"{reqData}"</span></span>
+    </div>
+  );
+}
+
+const MotionCardExample = ({ind} : any) => {
+  return (
+    <motion.div
+      className="w-[20rem] h-[20rem] p-2 m-2 flex flex-col justify-evenly items-center hover:shadow-xl rounded-md text-center bg-theme text-theme-w text-2xl font-bold uppercase transition-all duration-200"
+      initial={{ translateY:"40%", opacity: 0 }}
+      animate={{ translateY:0, opacity: 1 }}
+      transition={{ duration : 0.5 }}
+    >
+      <span className="inline-block text-xl">This is example use of framer motion Card Number : {ind}</span>
+      <button className="bg-theme-w text-theme font-bold hover:scale-105 rounded-md mx-auto p-2 px-4">Click Me: {ind}</button>
+    </motion.div>
+  )
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: 
+      {
+        "theme": "#5019e4",
+        "theme-alt": "#3007c2",
+        "theme-cont" : "#FA4032",
+        "theme-cont-alt" : "#d93021",
+        "theme-w" : "#ffffff",
+        "theme-w-alt" : "#dedede",
+      },
+      fontFamily : 
+      {
+        "main" : '"Roboto", serif',
+        "main-a" : 'ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        "main-alt" : '"Ysabeau SC", serif;',
+      },
+      fontSize : 
+      {
+        "title" : "1.2rem",
+        "heading" : "1.9rem",
+      },
+    },
+  },
+  plugins: [],
+}
+
