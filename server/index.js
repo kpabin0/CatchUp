9d3661ca72e35a0f660bebb7f5b58358748634dc
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-12 22:55:45 +0545

    add: news, tournaments temporary routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 
+import { newsJSON } from './routes/news.js'
+import { tournamentJSON } from './routes/tournament.js'
+
 const PORT_NUMBER = 8080
 
 const app = express()
@@ -11,8 +14,24 @@ app.listen(PORT_NUMBER, () => {
     console.log("Listening in Port number", PORT_NUMBER);
 })
 
+function wait(milliseconds) {
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
 app.get("/", (req, res) => {
     console.log("Request in server /");
 
     res.json("This is response json");
+})
+
+app.get("/news", async (req, res) => {
+    await wait(1000);
+    return await res.json(newsJSON);
+})
+
+app.get("/tournament", async (req, res) => {
+    await wait(1000);
+    return await res.json(tournamentJSON);
 })
---
+++ server/routes/news.js
@@ -0,0 +1,22 @@
+export const newsJSON = [
+    {
+        title: "This is news 1",
+        img: "",
+        description: "This is description of the news 1"
+    },
+    {
+        title: "This is news 2",
+        img: "",
+        description: "This is description of the news 2"
+    },
+    {
+        title: "This is news 3",
+        img: "",
+        description: "This is description of the news 3"
+    },
+    {
+        title: "This is news 4",
+        img: "",
+        description: "This is description of the news 4"
+    }
+]
