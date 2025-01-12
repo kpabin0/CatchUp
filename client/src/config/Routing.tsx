03b99a144a8d171a4f6d08850ecf3f9b53280941
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-12 22:57:59 +0545

    add: Routing Config

---
+++ client/src/config/Routing.tsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom"
+import App from "../App"
+import Home from "../pages/Home"
+import Matches from "../pages/Matches"
+import Tournaments from "../pages/Tournaments"
+import News from "../pages/News"
+import Fixtures from "../pages/Fixtures"
+import NotFound from "../pages/NotFound"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+const Routing = () => {
+  return (
+    <>
+    <Navbar />
+    <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<Home />}/>
+        <Route path="/matches" element={<Matches />}/>
+        <Route path="/tournaments" element={<Tournaments />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/fixtures" element={<Fixtures />} />
+
+        <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+    </>
+  )
+}
+
+export default Routing
