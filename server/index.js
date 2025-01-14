a116ffade0398d91aef6647aae40c1a3b87a6d9d
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-14 10:10:24 +0545

    fix: server listen address config

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./config/sql.config');
 
 const server = http.createServer(app);
-server.listen(5000, '127.0.0.6', () => {
+server.listen(5000, '127.0.0.1', () => {
   console.log('Server is running on port 5000');
   console.log('Press CTRL + C to disconnect');
 });
