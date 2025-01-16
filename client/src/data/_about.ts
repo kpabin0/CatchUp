144fa47f20bf0857c8399d426093b4ec66a5fd2a
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-16 09:44:37 +0545

    add: about for catchup

---
+++ client/src/data/_about.ts
@@ -0,0 +1,23 @@
+export const _about = {
+    name: "CatchUp",
+    quote: "Cricket for life",
+    address: "Thapathali, Kathmandu",
+    establishedIn: '2025-1-1',
+    personals: [
+        {
+            post: "Head of Department",
+            name: "Name Here hod",
+            img: "#"
+        },
+        {
+            post: "Assitant",
+            name: "Name Here a",
+            img: "#"
+        },
+        {
+            post: "Deputy Head",
+            name: "Name Here dh",
+            img: "#"
+        },
+    ]
+}
