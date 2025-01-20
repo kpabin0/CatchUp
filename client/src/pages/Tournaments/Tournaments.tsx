bea2d3e45ef8a5627bd487d888c6423ea32428cd
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-20 21:37:03 +0545

    update: datas displayed on table tr

--- client/src/pages/Tournaments/Tournaments.tsx
+++ client/src/pages/Tournaments/Tournaments.tsx
@@ -1,27 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-// import { getFallbackTournaments } from "../../data/_tournaments";
-import { ITournament } from "../../data/ITypes";
-import { MdDeleteForever, MdEdit } from "react-icons/md";
+// import { useNavigate } from "react-router-dom";
+import { getFallbackTournaments } from "../../data/_tournaments";
+import { ITournamentPointsTable } from "../../data/ITypes";
 
 const PORT_NUMBER = process.env.REACT_APP_PORT_NUMBER;
 
-interface ITournamentCard extends ITournament {
-  handleEdit: (id : number) => void,
-  handleDelete: (id : number) => void
-};
 
 const Tournaments = () => {
-  const [tournaments, setTournaments] = useState<ITournament[]>([]);
+  const [tournaments, setTournaments] = useState<ITournamentPointsTable[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [message, setMessage] = useState<string | null>(null);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   useEffect(() => {
     fetchTournaments();
-    // setTournaments(getFallbackTournaments())
   }, []);
 
   const fetchTournaments = async () => {
@@ -31,69 +25,72 @@ const Tournaments = () => {
       setMessage(null);
     } catch (error: any) {
       setError("Error fetching tournaments.");
+      setTournaments(getFallbackTournaments());
+      setError(null);
       console.error(error);
     }
   };
 
 
-  const handleDelete = async (tournamentid: number) => {
-    try {
-      const response = await axios.delete(
-        `http://localhost:${PORT_NUMBER}/tournaments/delete/${tournamentid}`
-      );
-      console.log(response.data); 
-      setMessage("Tournament deleted successfully.");
-      fetchTournaments(); 
-    } catch (error: any) {
-      console.error("Error deleting tournament:", error);
-      setError(error.response?.data.error || "Error deleting tournament.");
-      // navigate("/tournaments");
-    }
-  };
+  // const handleDelete = async (tournamentid: number) => {
+  //   try {
+  //     const response = await axios.delete(
+  //       `http://localhost:${PORT_NUMBER}/tournaments/delete/${tournamentid}`
+  //     );
+  //     console.log(response.data); 
+  //     setMessage("Tournament deleted successfully.");
+  //     fetchTournaments(); 
+  //   } catch (error: any) {
+  //     console.error("Error deleting tournament:", error);
+  //     setError(error.response?.data.error || "Error deleting tournament.");
+  //     navigate("/tournaments");
+  //   }
+  // };
 
 
-  const handleEdit = (tournamentid: number) => {
-    navigate(`/tournaments/edit/${tournamentid}`);
-  };
+  // const handleEdit = (tournamentid: number) => {
+  //   navigate(`/tournaments/edit/${tournamentid}`);
+  // };
 
   return (
     <section className="w-full h-screen flex justify-center items-center">
-      <div className="w-[40rem] flex flex-col items-center shadow-xl rounded-lg bg-white">
+      <div className="p-6 flex flex-col items-center border-2 shadow-xl rounded-lg bg-white">
         <h2 className="w-full py-6 text-2xl font-extrabold uppercase bg-theme text-theme-w text-center mb-5">All Tournaments</h2>
 
         {message && <div className="text-theme-green mb-4">{message}</div>}
         {error && <div className="text-theme-cont mb-4">{error}</div>}
 
-        <ul className="space-y-4 w-full p-4">
-          {tournaments.map((tournament) => {
+        <table className="text-center border border-theme">
+          <tr className="border border-theme">
+            <th>Name</th>
+            <th>Matches Played</th>
+            <th>Matches Won</th>
+            <th>Matches Tied</th>
+            <th>Matches Loss</th>
+            <th>Points</th>
+          </tr>
+          {tournaments.map((tournament, ind) => {
             return (
-              <TournamentCard key={tournament.tournamentid} {...tournament} handleEdit={handleEdit} handleDelete={handleDelete}  />
+              <TournamentCard key={ind} {...tournament} />
             )
           })}
-        </ul>
+        </table>
       </div>
     </section>
-  );
+  )
 };
 
-const TournamentCard = ({tournamentid, name, start_date, end_date, handleEdit, handleDelete} : ITournamentCard) => {
+const TournamentCard = ({team_name, matches_played, matches_won, matches_tied, points} : ITournamentPointsTable) => {
   return (
-      <li className="p-4 border rounded-lg shadow-sm hover:border-theme flex flex-row justify-between items-center">     
-        <div className="flex flex-col">
-          <strong>ID {tournamentid}</strong>
-          <strong>Name: {name}</strong>
-          <span>Start Date: {new Date(start_date).toLocaleDateString()}</span>
-          <span>End Date: {new Date(end_date).toLocaleDateString()}</span>
-        </div>
-        <div className="flex flex-col space-y-5">
-          <button onClick={() => handleEdit(tournamentid)} className="bg-theme text-theme-w px-4 py-2 rounded">
-            <MdEdit />
-          </button>
-          <button onClick={() => handleDelete(tournamentid)} className="bg-theme-cont text-theme-w px-4 py-2 rounded">
-            <MdDeleteForever />
-          </button>
-        </div>
-      </li>
+    <tr className="my-2 py-2 text-sm">
+      <td>{team_name}</td>
+      <td>{matches_played}</td>
+      <td>{matches_won}</td>
+      <td>{matches_tied}</td>
+      <td>{matches_played - matches_won - matches_tied}</td>
+      <td>{points}</td>
+    </tr>
+
   )
 }
 
