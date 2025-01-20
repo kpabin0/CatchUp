28220a3beb702dd904538f455a67e238bbf9b9d6
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-01-20 21:31:36 +0545

    add: fallback function added for fixturesdata

---
+++ client/src/data/_fixtures.ts
@@ -0,0 +1,28 @@
+// This file contain the fallback methods with data incase of fetch failure
+
+import { getArray } from "./utils"
+
+export function getFallbackFixtures()
+{
+    const matchCount = 5
+    const tempMatch = getArray(matchCount)
+  
+    return tempMatch.map((ind) => {
+      return {
+        team_1: {
+          name: "Fallback - " + ind.toString(),
+          runs: ind * 2,
+          wickets: ind,
+          over: ind/2,
+        },
+        team_2: {
+          name: "Fallback - " + (ind + 1).toString(),
+          runs: ind * 4,
+          wickets: ind,
+          over: ind/2 - 0.2,
+        },
+        isLive: ind % 2 === 1 ? true : false,
+        date: "2025-10-2"
+      }
+    })
+}
