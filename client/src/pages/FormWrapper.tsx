3d670ccbeef89074171aac65ec8a06bc8f27dd4f
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-02-02 19:32:52 +0545

    update: framer div added

--- client/src/pages/FormWrapper.tsx
+++ client/src/pages/FormWrapper.tsx
@@ -1,11 +1,22 @@
+import { motion } from 'framer-motion'
+
 const FormWrapper = (props: any) => {
 
     return (
       <section className="min-h-screen min-w-full flex flex-col justify-evenly items-center">
-          <div className={"min-w-[25rem] p-4 bg-theme-w shadow-xl rounded-md border " + (props?.ostyle)}>
-              <h1 className="text-3xl text-center my-6 text-theme uppercase font-extrabold">{props?.title ? props.title : "Form"}</h1>
-              {props?.children}
-          </div>
+        <motion.div 
+          className={"rounded-xl shadow-xl min-w-[25rem] min-h-[50vh] flex flex-col justify-between items-center bg-theme-w "  + (props.ostyle ? props.ostyle : "")}
+          initial={{ opacity: 0, translateY: 50 }}
+          whileInView={{ opacity: 1, translateY: 0 }}
+          viewport={{once : false}}
+          transition={{
+              duration: 0.5,
+              delay: 0.1
+          }}
+        >
+          {props.title ? <h1 className="text-3xl text-center my-6 text-theme uppercase font-extrabold">{props?.title}</h1> :<></>}
+          {props.children}
+        </motion.div>
       </section>
     );
   };
