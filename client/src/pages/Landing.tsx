676d10675efb09d5a96d353b5e8dd28b5f19f2dd
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-02-03 19:09:52 +0545

    update: tournament name fetched from backend with fallback

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,34 +1,56 @@
 import { useEffect, useState } from 'react'
 import ThemeLink from '../components/ThemeLink'
 import BasicDiv from '../components/BasicDiv';
+import { AxiosGet } from '../utils/utils';
+import { useInfoHandler } from '../customhook/info';
 
 const Landing = () => {
 
-  const _tournamentNames = ["Nepal Premier League", "Elite Cup", "Jay Nepal Cup"];
+  const [tournaments, setTournament] = useState<string[]>([]);
   const [textCount, setTextCount] = useState<number>(-1);
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const [currentTournament, setCurrentTournament] = useState<string>("");
 
+  const { info, setInfo } = useInfoHandler();
 
   useEffect(() => {
+    // currently this is hardcoded and not typesafe
+    const forwardSetTournament = (d: any) => {
+      const _tournaments: string[] = []
+      d.forEach((i: any) => { _tournaments.push(i.name)})
+      // console.log(_tournaments)
+      setTournament(_tournaments);
+    }
+
+    // this is fallback incase fetch failed;
+    setTournament(["Nepal Premier League", "Elite Cup", "Jay Nepal Cup"]);
+    AxiosGet(`/tournaments`, forwardSetTournament, setInfo);
+    // console.log(info);
+  }, [])
+  
+  useEffect(() => {
+    if(tournaments.length === 0) return;
+    console.log(tournaments);
     let textC = 0, activeInd = 0;
-    setInterval(() => {
+    const interval = setInterval(() => {
       textC++;
-      if(textC > _tournamentNames[activeInd].length)
+      if(textC > tournaments[activeInd].length)
       {
-        activeInd = (activeInd+1) % _tournamentNames.length;
+        activeInd = (activeInd+1) % tournaments.length;
         textC = 0;
       }
 
       setTextCount(textC);
       setActiveIndex(activeInd);
-    }, 5000/_tournamentNames[activeInd].length);
+    }, 5000/tournaments[activeInd].length);
+
+    return () => clearInterval(interval)
     
     // eslint-disable-next-line
-  }, [])
+  }, [tournaments])
 
   useEffect(()=> {
-    setCurrentTournament(_tournamentNames[activeIndex])
+    setCurrentTournament(tournaments[activeIndex] || "Loading")
 
     // eslint-disable-next-line
   },[activeIndex])
