04e7aaf0ec78ab62e99a7ed62d8ed098969f64eb
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-02-06 16:15:15 +0545

    add: table template wrapper (temp)

---
+++ client/src/pages/TableTemplate.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react'
+import { motion } from 'framer-motion'
+
+interface ITable {
+  title?: string,
+  th: string[],
+  rd?: any[],
+  ostyle?: string,
+  // this is for the special entry in table or say action
+  control?: any
+};
+
+const TableTemplate = ({title, th, rd, ostyle, control}: ITable) => {
+
+  return (
+    <section className="min-w-full flex flex-col justify-evenly items-center">
+      {title ? <h1 className="text-2xl text-center my-10 text-theme uppercase font-bold">{title}</h1> :<></>}
+        
+      <motion.table
+        className={"min-w-[60%] rounded-md overflow-hidden text-left bg-theme-w-alt"  + (ostyle ? ostyle : "")}
+        initial={{ opacity: 0, translateY: 50 }}
+        whileInView={{ opacity: 1, translateY: 0 }}
+        viewport={{once : false}}
+        transition={{
+            duration: 0.5,
+            delay: 0.1
+        }}
+      >
+      <thead className="w-full uppercase">
+        {
+          th ? th.map((title, ind) => {
+            return <th key={ind} className="p-4 border-b text-theme border-theme-cont">{title}</th>
+          }):<></>
+        }
+        {
+          control ? <th className="p-4 border-b text-theme border-theme-cont">Action</th> : <></>
+        }
+      </thead>
+      <tbody className="w-full">
+        {rd ? rd.map((props, ind) => {
+          return <TableRow key={ind} props={props} control={control} />
+        }):<></>}
+      </tbody>
+          
+      </motion.table>
+    </section>
+  )
+}
+
+const TableRow = ({props, control} : {props: Object, control: any}) => {
+  return (
+    <motion.tr 
+      initial={{ translateY: 50 }}
+      whileInView={{ translateY: 0 }}
+      viewport={{once : true}}
+      transition={{
+          duration: 0.5,
+          delay: 0.1 
+      }}
+      className="w-full p-4 border-b border-theme-g">
+      {
+        Object.entries(props).map(([k, v], ind) => {
+          return <td key={ind} className="p-4 ">{v}</td>
+        })
+      }
+      {control ? <td className="p-4 ">{control}</td>:<></>}
+    </motion.tr>
+  )
+}
+
+export default TableTemplate
